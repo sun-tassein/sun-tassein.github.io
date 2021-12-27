0d0f6eaaa78bdd03ff532e9e7d38a48f853129e9
sun-tassein <96750101+sun-tassein@users.noreply.github.com>
2021-12-27 22:59:01 +0100

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+let selectedFile;//input file je polje za putanju dodeljujemo selectedFile
+document.getElementById("input").addEventListener("change", (event) => {selectedFile = event.target.files[0];});
+
+document.getElementById("button").addEventListener("click", () => {
+  if (selectedFile) {
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      let data = event.target.result;
+      let workbook = XLSX.read(data, { type: "binary" });
+      let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets["Sheet1"]); //sheet]);
+      console.log(rowObject);
+      document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4);
+    };
+  }
+});
