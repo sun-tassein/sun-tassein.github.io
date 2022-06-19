de6e4502bd6cb062a4d52fe78d89ad445c5595da
sun-tassein <96750101+sun-tassein@users.noreply.github.com>
2022-06-19 02:19:47 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,12 @@ document.getElementById("button").addEventListener("click", () => {
     };
   }
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
